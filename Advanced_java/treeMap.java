7218f317ad4f75db720b6cc29f57259bb128c159
Tsehay Goremes <ttsehay499@gmail.com>
2023-12-15 16:41:20 +0300

    #

--- Advanced_java/treeMap.java
+++ Advanced_java/treeMap.java
@@ -1,12 +1,12 @@
 import java.util.*;
 public abstract class treeMap {
     public static void main(String[] args) {
-        TreeMap<Integer,String> map= new TreegetValue(Map<Integer,String>();
-        map.put(1,"SE");
-        map.put(2,"CSE");
+        TreeMap<Integer,String> map= new TreeMap<Integer,String>();
+        map.put(1,". SE");
+        map.put(2,". CSE");
 
-        for (map.Entry m:map.entrySEt()){
-            System.out.println(m.getKey( +""+m.));
+        for (Map.Entry m:map.entrySet()){
+            System.out.println(m.getKey()+""+m.getValue());
         }
     }
 }

710369228b3932c788f9cb3568ecd5b2e60cd592
Tsehay Goremes <ttsehay499@gmail.com>
2023-12-15 14:58:21 +0300

    create treeMap

---
+++ Advanced_java/treeMap.java
@@ -0,0 +1,11 @@
+public abstract class treeMap {
+    public static void main(String[] args) {
+        TreeMap<Integer,String> map= new TreegetValue(Map<Integer,String>();
+        map.put(1,"SE");
+        map.put(2,"CSE");
+
+        for (map.Entry m:map.entrySEt()){
+            System.out.println(m.getKey( +""+m.));
+        }
+    }
+}

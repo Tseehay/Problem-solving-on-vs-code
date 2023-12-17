fdea9fdf0dcae0964f5ccffe4eed824fd4cf378d
Tsehay Goremes <ttsehay499@gmail.com>
2023-12-17 13:24:27 +0300

    add comment

--- Advanced_java/eample JDBC/Example1.java
+++ Advanced_java/eample JDBC/Example1.java
@@ -5,13 +5,17 @@ class JDatabase
 {  
 public static void main(String args[]){  
 try{  
+    // 1. register
 Class.forName("com.mysql.jdbc.Driver");  
   
+    // 2. connection
 Connection con=DriverManager.getConnection(  
 "jdbc:mysql://localhost:3306/student","root","root");  
   
+    // 3. crt statment
 Statement stmt=con.createStatement();  
   
+    // 4. excute query 
 ResultSet rs=stmt.executeQuery("select * from stud");  
  System.out.println("ID\t\tName\tGender\tDept");
  System.out.print("---------------------------------------\n");
@@ -21,7 +25,7 @@ System.out.println(rs.getString(1)+"\t"+rs.getString(2)+"\t"+rs.getString(3)
         +"\t"+rs.getString(4)); 
 }
 System.out.println("---------------------------------------");
-  
+    // 5. close
 con.close();  
   
 }catch(Exception e)

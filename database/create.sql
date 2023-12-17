2bf3e4458a3f4a14c07dd908ebde18fa21d43352
Tsehay Goremes <ttsehay499@gmail.com>
2023-12-17 10:01:27 +0300

    person table

--- database/create.sql
+++ database/create.sql
@@ -1,7 +1,14 @@
 CREATE DATABASE data;
 
-CREATE TABLE table_name (
-    column1 datatype,
-    column2 datatype,
-    column3 datatype,
+
+CREATE TABLE Persons (
+    PersonID int,
+    LastName varchar(255),
+    FirstName varchar(255),
+    Address varchar(255),
+    City varchar(255)
 );
+
+SELECT * from  Persons;
+
+ 

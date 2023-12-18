b9a109274f28c852e6645f9ada65668b4b61c7d3
Tsehay Goremes <ttsehay499@gmail.com>
2023-12-18 17:11:44 +0300

    #

--- Advanced_java/eample JDBC/Example11.java
+++ Advanced_java/eample JDBC/Example11.java
@@ -1,7 +1,7 @@
 
 import java.sql.*;  
 
-class JDatabase
+class Example11
 {  
 public static void main(String args[]){  
 try{  
--- Advanced_java/generic/testGeneric.java
+++ Advanced_java/generic/testGeneric.java
@@ -1,6 +1,6 @@
 public class testGeneric {
     public static void main(String args[]){
-            MyGen<Integer> g1=new MyGen<Integer>();
+            MyGen<T> g1=new MyGen<T>();
             g1.add(2);
             //g1.add("CSE");//Compile time error  
             System.out.println(g1.get());

4cfd1d2222c2cf1acc9de921db1be59188a80a74
Tsehay Goremes <ttsehay499@gmail.com>
2023-12-18 18:15:26 +0300

    #

--- Advanced_java/generic/testGeneric.java
+++ Advanced_java/generic/testGeneric.java
@@ -1,9 +1,23 @@
+
+import java.util.ArrayList;
+
 public class testGeneric {
     public static void main(String args[]){
-            MyGen<T> g1=new MyGen<T>();
+            MyGen<Integer> g1=new MyGen<Integer>();
             g1.add(2);
-            //g1.add("CSE");//Compile time error  
+            //g1.add("CSE");//Compile time error  
             System.out.println(g1.get());
     }
 }
 
+class MyGen<T> {
+    private ArrayList<T> list = new ArrayList<>();
+
+    public void add(T t) {
+        list.add(t);
+    }
+
+    public T get() {
+        return list.get(0);
+    }
+}
